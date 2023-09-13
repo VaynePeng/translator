e03fbf286d0d2c17833e51e74d6d81f73a7cd8a2
VaynePeng <vaynepeng@gmail.com>
2023-09-13 15:10:59 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const message = ref<string>('hello world')
+</script>
+
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<style scoped>
+</style>
