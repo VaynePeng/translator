41b5de69e7880b873b6eef267e23fa0cc6d534eb
VaynePeng <vaynepeng@gmail.com>
2023-09-22 16:27:09 +0800

    fix: 修改eslint

--- src/app.vue
+++ src/app.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import NavBar from '@/components/nav-bar.vue'
+import ThemeCover from '@/components/theme-cover.vue';
 </script>
 
 <template>
+  <ThemeCover />
   <div class="w-[100vw] h-[100vh] overflow-hidden bg-white dark:bg-slate-800">
     <NavBar />
     <main>MAIN</main>
---
+++ src/components/theme-cover.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="fixed left-[-50%] top-[-50%] translate-x-1/2 translate-y-1/2 w-10 h-10 rounded-[50%] bg-slate-800"
+  ></div>
+</template>
