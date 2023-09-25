f888e6fe1501eb64e5713c427373683be3fb540d
VaynePeng <vaynepeng@gmail.com>
2023-09-25 17:50:39 +0800

    chore: change tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: 'class',
-  content: ['./src/**/*.{vue,jsx,tsx}', './index.html'],
+  content: ['./src/**/*.{vue,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {}
-    }
+    colors: {},
+    extend: {}
   },
   plugins: []
 }
