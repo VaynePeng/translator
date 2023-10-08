a3f115042849f069faa4a7380ed883bc36520fab
VaynePeng <vaynepeng@gmail.com>
2023-10-08 16:29:54 +0800

    chore: 修改项目结构

--- src/components/TransInput.vue
+++ src/components/TransInput.vue
@@ -2,6 +2,6 @@
 
 <template>
   <textarea
-    class="border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 w-full dark:bg-slate-600 px-2 py-2 min-h-[10rem]"
+    class="border border-slate-300 rounded-md shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 w-full dark:bg-slate-600 px-2 py-2 min-h-[10rem]"
   ></textarea>
 </template>
---
+++ src/components/TransOutput.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="rounded-md shadow-sm w-full bg-slate-300 dark:bg-slate-800 px-2 py-2 min-h-[10rem]"
+  ></div>
+</template>
